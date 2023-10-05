3a46b0073fe2232f65b7117471acb88663528147
GameDev <twintower9875@gmail.com>
2023-10-05 16:12:40 -0500

    Added Feed Posts Route/Container

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -38,96 +38,124 @@ const createPost = async (req, res) => {
 };
 
 const getPost = async (req, res) => {
-    try{
-        const post = await Post.findById(req.params.id)
+  try {
+    const post = await Post.findById(req.params.id);
 
-        if(!post){
-            return res.status(404).json({ message: "Post not found"})
-        }
-
-        res.status(200).json({ post })
-    }catch(error){
-        res.status(500).json({ message: error.message });
-        console.log("Error in: getPost: ", error.message);
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
     }
-}
+
+    res.status(200).json({ post });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+    console.log("Error in: getPost: ", error.message);
+  }
+};
 
 const deletePost = async (req, res) => {
-    try{
-        const post = await Post.findById(req.params.id);
-        if(!post){
-            return res.status(404).json({ message: "Post not found" });
-        }
-
-        if(post.postedBy.toString() !== req.user._id.toString()){
-            return res.status(401).json({ message: "Unauthorized to delete post" });
-        }
-
-        await Post.findByIdAndDelete(req.params.id);
-
-        res.status(200).json({ message: "Post deleted successfully" }); 
-    } catch(error){
-        res.status(500).json({ message: error.message });
-        console.log("Error in: deletePost: ", error.message);
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
     }
-}
+
+    if (post.postedBy.toString() !== req.user._id.toString()) {
+      return res.status(401).json({ message: "Unauthorized to delete post" });
+    }
+
+    await Post.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({ message: "Post deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+    console.log("Error in: deletePost: ", error.message);
+  }
+};
 
 const likeUnlikePost = async (req, res) => {
-    try{    
-        const { id:postId } = req.params;
-        const userId = req.user._id;
+  try {
+    const { id: postId } = req.params;
+    const userId = req.user._id;
 
-        const post = await Post.findById(postId);
+    const post = await Post.findById(postId);
 
-        if(!post) {
-            return res.status(404).json({ message: "Post not found" })
-        }
-
-        const userLikedPost = post.likes.includes(userId);
-
-        if(userLikedPost){
-            //Unlike post
-            await Post.updateOne({_id:postId}, {$pull: {likes: userId}});
-            res.status(200).json({message: "Post unliked successfully"});
-        } else{
-            // Like post
-            post.likes.push(userId);
-            await post.save();
-            res.status(500).json({ message: "Post liked successfully"})
-        }
-    }catch(error){
-        res.status(500).json({ message: error.message });
-        console.log("Error in: likeUnlikePost: ", error.message);
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
     }
-}
+
+    const userLikedPost = post.likes.includes(userId);
+
+    if (userLikedPost) {
+      //Unlike post
+      await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
+      res.status(200).json({ message: "Post unliked successfully" });
+    } else {
+      // Like post
+      post.likes.push(userId);
+      await post.save();
+      res.status(500).json({ message: "Post liked successfully" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+    console.log("Error in: likeUnlikePost: ", error.message);
+  }
+};
 
 const replyToPost = async (req, res) => {
-    try{
-        const {text} = req.body;
-        const postId = req.params.id;
-        const userId = req.user._id;
-        const userProfilePic = req.user.profilePic;
-        const username = req.user.username;
+  try {
+    const { text } = req.body;
+    const postId = req.params.id;
+    const userId = req.user._id;
+    const userProfilePic = req.user.profilePic;
+    const username = req.user.username;
 
-        if(!text){
-            return res.status(400).json({ message: "Text field is required"})
-        }
-
-        const post = await Post.findById(postId);
-        if(!post){
-            return res.status(404).json({ message: "Post not found"})
-        }
-
-        const reply = {userId, text, userProfilePic, username};
-
-        post.replies.push(reply);
-        await post.save();
-
-        res.status(200).json({ message: "Reply added successfully", post });
-    } catch(error){
-        res.status(500).json({ message: error.message });
-        console.log("Error in: replyToPost: ", error.message);
+    if (!text) {
+      return res.status(400).json({ message: "Text field is required" });
     }
-}
 
-export { createPost, getPost, deletePost, likeUnlikePost, replyToPost };
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    const reply = { userId, text, userProfilePic, username };
+
+    post.replies.push(reply);
+    await post.save();
+
+    res.status(200).json({ message: "Reply added successfully", post });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+    console.log("Error in: replyToPost: ", error.message);
+  }
+};
+
+const getFeedPosts = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const following = user.following;
+
+    const feedPosts = await Post.find({ postedBy: { $in: following } }).sort({
+      createdAt: -1,
+    });
+
+    res.status(200).json({ feedPosts });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+    console.log("Error in: getFeedPosts: ", error.message);
+  }
+};
+
+export {
+  createPost,
+  getPost,
+  deletePost,
+  likeUnlikePost,
+  replyToPost,
+  getFeedPosts,
+};
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,11 +1,19 @@
 import express from "express";
-import { createPost, getPost, deletePost, likeUnlikePost, replyToPost } from "../controllers/postController.js";
+import {
+  createPost,
+  getPost,
+  deletePost,
+  likeUnlikePost,
+  replyToPost,
+  getFeedPosts,
+} from "../controllers/postController.js";
 import { protectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
 // GET
-router.get("/:id", getPost)
+router.get("/feed", protectRoute, getFeedPosts);
+router.get("/:id", getPost);
 
 // POST
 router.post("/create", protectRoute, createPost);
@@ -13,6 +21,6 @@ router.post("/like/:id", protectRoute, likeUnlikePost);
 router.post("/reply/:id", protectRoute, replyToPost);
 
 // DELETE
-router.delete("/:id", protectRoute, deletePost)
+router.delete("/:id", protectRoute, deletePost);
 
 export default router;
